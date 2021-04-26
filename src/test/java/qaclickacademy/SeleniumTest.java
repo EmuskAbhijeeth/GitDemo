690815427453fdd0ed6d82064eeffea318a67207
EmuskAbhijeeth <abhijeethvedula56@gmail.com>
2021-04-26 16:33:58 +0530

    changed by USA Architect

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -8,6 +8,20 @@ public class SeleniumTest {
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAutomation");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira");
 	}
 	
 	@Test
